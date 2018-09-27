b6a3182318d8d7a077c6a776c45c4409ca62d54b
sunmouren <sunxuechao1024@gmail.com>
2018-09-27 21:37:22 +0800

    完成个人主页前后端数据对接。

--- comments/views.py
+++ comments/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-class UserProfileView(View):
-    def get(self, request):
-        return render(request, 'book-list.html')
+
--- users/views.py
+++ users/views.py
@@ -5,8 +5,29 @@ from .models import UserProfile
 
 
 class UserProfileView(View):
+    """
+    用户个人主页
+    """
     def get(self, request, user_id):
         user = get_object_or_404(UserProfile, id=int(user_id))
+        is_owner = '我' if request.user == user else 'TA'
+        data = request.GET.get('data', 'books')
+        books = self.get_books(data, user)
+        comments = self.get_comments(data, user)
+        print(books)
+        print(comments)
+
         return render(request, 'user-profile.html', {
             'user': user,
+            'books': books,
+            'comments': comments,
+            'is_owner': is_owner,
+            'data': data,
         })
+
+    def get_books(self, data, user):
+        return user.upload_books.all() if data == 'books' else None
+
+    def get_comments(self, data, user):
+        return user.book_comments.all() if data == 'comments' else None
+
